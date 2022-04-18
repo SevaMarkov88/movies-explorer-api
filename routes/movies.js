a0ca1f87ecce5fae6f0b86df277db255704e3309
SevaMarkov88 <vsevolodmarkoff@yandex.ru>
2022-04-18 12:02:19 +0300

    controllers added

--- routes/movies.js
+++ routes/movies.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const {
   getMovies,
-  postMovies,
-  deleteMovies,
+  postMovie,
+  deleteMovie,
 } = require('../controllers/movies');
 const { validateIdParameter } = require('../middlewares/validationJoi');
 
 router.get('/', auth, getMovies);
-router.post('/', auth, postMovies);
-router.delete('/_id', auth, validateIdParameter('movieId'), deleteMovies);
+router.post('/', auth, postMovie);
+router.delete('/_id', auth, validateIdParameter('movieId'), deleteMovie);
 
 module.exports = router;
