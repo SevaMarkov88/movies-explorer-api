adf66deacd6482445b677a75353d1979a87ca0f4
SevaMarkov88 <vsevolodmarkoff@yandex.ru>
2022-05-20 16:41:31 +0300

    login fixed v1.2

--- controllers/users.js
+++ controllers/users.js
@@ -84,7 +84,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         });
-      res.status(201).send({ message: 'Login successful' });
+      res.status(201).send({ token });
     })
     .catch(next);
 };
