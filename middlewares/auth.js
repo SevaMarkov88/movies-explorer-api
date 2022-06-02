4c32a438400eaf93f8c49560712a2299a7a9b7e8
Seva Markov <88732338+SevaMarkov88@users.noreply.github.com>
2022-06-02 16:22:27 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,27 @@
+require('dotenv').config();
+
+const { JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/UnauthorizedError');
+const UnauthorizedError = require('../errors/unauthorized-error');
+const errorMessages = require('../errors/messages');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
 
-module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError(errorMessages.unauthorized);
+  }
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    next(new UnauthorizedError('Необходима авторизация'));
+    throw new UnauthorizedError(errorMessages.unauthorized);
   }
 
   req.user = payload;
-
   next();
 };
+
+module.exports = auth;
